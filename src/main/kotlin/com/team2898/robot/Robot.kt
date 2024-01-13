e9aaf4554ebe740b3f5daeabcc064c8dfd184822
Anthony <anthonyvirayav@gmail.com>
2024-01-12 18:58:15 -0800

    Now using 2024 WPILIB and vendordeps

--- src/main/kotlin/com/team2898/robot/Robot.kt
+++ src/main/kotlin/com/team2898/robot/Robot.kt
@@ -5,6 +5,7 @@ package com.team2898.robot
 
 import com.team2898.robot.commands.TeleOp
 import com.team2898.robot.subsystems.Drivetrain
+import com.team2898.robot.subsystems.Shooter
 import edu.wpi.first.wpilibj.TimedRobot
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandScheduler
@@ -26,6 +27,7 @@ class Robot : TimedRobot() {
      */
     override fun robotInit() {
         Drivetrain
+        Shooter
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         robotContainer = RobotContainer()
